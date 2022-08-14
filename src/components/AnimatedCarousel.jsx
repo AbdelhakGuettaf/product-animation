65797942d2e50c249b1bdf97c4bc9d3e873c4314
Shocky <hakotaf@gmail.com>
2022-08-14 20:33:19 +0100

    Main animation and logic

--- src/components/AnimatedCarousel.jsx
+++ src/components/AnimatedCarousel.jsx
@@ -8,10 +8,26 @@ import {
 } from "../assets/svgs.js";
 
 export default function AnimatedCarousel() {
-  const [className, changeClass] = useState("one");
+  const [rotation, rotate] = useState(0);
+  const styles = [
+    {
+      container: { transform: `rotate(${rotation}deg)` },
+      orbitOne: { transform: `rotate(${rotation}deg)` },
+      background: { backgroundPosition: "0% 0%" },
+    },
+    {
+      container: { transform: `rotate(${rotation}deg)` },
+      orbitOne: { transform: `rotate(${rotation}deg)` },
+      background: { backgroundPosition: "34% 16%" },
+    },
+  ];
+  const [position, changePosition] = useState(0);
   const divRef = useRef(null);
   const Click = () => {
-    changeClass("two");
+    rotate(rotation + 180);
+    changePosition((prevState) =>
+      prevState < styles.length - 1 ? prevState + 1 : 0
+    );
   };
   return (
     <div>
@@ -20,12 +36,46 @@ export default function AnimatedCarousel() {
           Click();
         }}
       >
-        {className}
+        {position}
       </button>
       <div id="carousel-wrapper">
-        <div id="recCont" ref={divRef}>
-          {RevRec(className)}
-          <div id="rec_bg" className={className}></div>
+        <div id="wrapper">
+          <div
+            id="rec-container"
+            style={styles[position].container}
+            ref={divRef}
+          >
+            {RevRec("tes")}
+            <div id="rec_bg" style={styles[position].background}></div>
+          </div>
+        </div>
+        <div id="orbit-container" style={styles[position].orbitContainer}>
+          <div
+            style={{
+              ...styles[position].orbitOne,
+              height: "100%",
+              margin: "0 50%",
+            }}
+          >
+            {Pyramid()}
+            <div
+              style={{
+                height: "100%",
+                rotate: "90deg",
+              }}
+            >
+              {Sphere()}
+            </div>
+            <div
+              style={{
+                height: "100%",
+                padding: "60px",
+                rotate: "250deg",
+              }}
+            >
+              {Sphere()}
+            </div>
+          </div>
         </div>
       </div>
     </div>
